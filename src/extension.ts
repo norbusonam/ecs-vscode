38fb43b90d90c7e7cbf73150e02f07ed5be18a81
norbusonam <nsonam1822@gmail.com>
2023-11-29 22:03:14 -0500

    on hover over useFeatureGate, parse fg and show value

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,62 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "ecs-vscode" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('ecs-vscode.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ECS VSCode!');
-	});
-
-	context.subscriptions.push(disposable);
+async function getFeatureGateValues() {
+  try {
+    const featureGateUrl = vscode.workspace
+      .getConfiguration()
+      .get("ecs-vscode.ecsUrl") as string;
+    const jsonResponsePath = vscode.workspace
+      .getConfiguration()
+      .get("ecs-vscode.jsonResponsePath") as string;
+    const featureGateResponse = await fetch(featureGateUrl);
+    const featureGateJson = (await featureGateResponse.json()) as any;
+    const featureGateValues = featureGateJson[jsonResponsePath];
+    if (!featureGateValues) {
+      throw new Error("no feature gate values found");
+    }
+    return featureGateValues;
+  } catch {
+    vscode.window.showErrorMessage(
+      "Unable to fetch feature gate values. Please check your settings."
+    );
+  }
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export async function activate(context: vscode.ExtensionContext) {
+  const featureGateValues = await getFeatureGateValues();
+
+  const hover = vscode.languages.registerHoverProvider(
+    ["javascript", "typescript", "javascriptreact", "typescriptreact"],
+    {
+      provideHover(document, position, token) {
+        // check if the word is useFeatureGate
+        const range = document.getWordRangeAtPosition(position);
+        const word = document.getText(range);
+        if (word === "useFeatureGate") {
+          // check if feature name is on the same line
+          const line = document.lineAt(position);
+          let featureName = line.text.match(
+            /useFeatureGate\(['"](.*)['"]\)/
+          )?.[1];
+
+          // if it's not on the same line, check the next line
+          if (!featureName) {
+            const nextLine = document.lineAt(position.line + 1);
+            featureName = nextLine.text.match(/['"](.*)['"]/)?.[1];
+          }
+
+          // unable to find feature name
+          if (featureName) {
+            return new vscode.Hover(
+              `\`${featureName}\`: ${featureGateValues[featureName]}`
+            );
+          } else {
+            return new vscode.Hover("Couldn't parse the feature name :(");
+          }
+        }
+      },
+    }
+  );
+
+  context.subscriptions.push(hover);
+}
